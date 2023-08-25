87e1196eacea04f9622aea711630836b88d71172
meuntouchable <dortey@outlook.de>
2023-08-25 12:13:58 +0200

    added custom mod resource

---
+++ newmodels/mod_list.lua
@@ -0,0 +1,124 @@
+modList={
+	ped={
+		--female
+		{id=100000,base_id=226,path="models/peds/female/",name="Female 1",metaDownloadFalse=true},
+		{id=100001,base_id=233,path="models/peds/female/",name="Female 2",metaDownloadFalse=true},
+		{id=100002,base_id=233,path="models/peds/female/",name="Female 3",metaDownloadFalse=true},
+		{id=100003,base_id=233,path="models/peds/female/",name="Female 4",metaDownloadFalse=true},
+		{id=100004,base_id=169,path="models/peds/female/",name="Female 5",metaDownloadFalse=true},
+		{id=100005,base_id=233,path="models/peds/female/",name="Female 6",metaDownloadFalse=true},
+		{id=100006,base_id=233,path="models/peds/female/",name="Female 7",metaDownloadFalse=true},
+		{id=100007,base_id=233,path="models/peds/female/",name="Female 8",metaDownloadFalse=true},
+		{id=100008,base_id=233,path="models/peds/female/",name="Female 9",metaDownloadFalse=true},
+		{id=100009,base_id=233,path="models/peds/female/",name="Female 10",metaDownloadFalse=true},
+		{id=100010,base_id=233,path="models/peds/female/",name="Female 11",metaDownloadFalse=true},
+		{id=100011,base_id=233,path="models/peds/female/",name="Female 12",metaDownloadFalse=true},
+		{id=100012,base_id=233,path="models/peds/female/",name="Female 13",metaDownloadFalse=true},
+		{id=100013,base_id=233,path="models/peds/female/",name="Female 14",metaDownloadFalse=true},
+		{id=100014,base_id=233,path="models/peds/female/",name="Female 15",metaDownloadFalse=true},
+		{id=100015,base_id=233,path="models/peds/female/",name="Female 16",metaDownloadFalse=true},
+		{id=100016,base_id=138,path="models/peds/female/",name="Female 17",metaDownloadFalse=true},
+		{id=100017,base_id=138,path="models/peds/female/",name="Female 18",metaDownloadFalse=true},
+		{id=100018,base_id=138,path="models/peds/female/",name="Female 19",metaDownloadFalse=true},
+		{id=100019,base_id=138,path="models/peds/female/",name="Female 20",metaDownloadFalse=true},
+		{id=100020,base_id=233,path="models/peds/female/",name="Female 21",metaDownloadFalse=true},
+		{id=100021,base_id=233,path="models/peds/female/",name="Female 22",metaDownloadFalse=true},
+		{id=100022,base_id=233,path="models/peds/female/",name="Female 23",metaDownloadFalse=true},
+		{id=100023,base_id=233,path="models/peds/female/",name="Female 24",metaDownloadFalse=true},
+		{id=100024,base_id=233,path="models/peds/female/",name="Female 25",metaDownloadFalse=true},
+		{id=100025,base_id=233,path="models/peds/female/",name="Female 26",metaDownloadFalse=true},
+		{id=100026,base_id=93,path="models/peds/female/",name="Female 27",metaDownloadFalse=true},
+		{id=100027,base_id=233,path="models/peds/female/",name="Female 28",metaDownloadFalse=true},
+		
+		--female donor
+		{id=110000,base_id=55,path="models/peds/female/donator/",name="Female Donor 1",metaDownloadFalse=true},
+		{id=110001,base_id=55,path="models/peds/female/donator/",name="Female Donor 2",metaDownloadFalse=true},
+		{id=110002,base_id=55,path="models/peds/female/donator/",name="Female Donor 3",metaDownloadFalse=true},
+		{id=110003,base_id=55,path="models/peds/female/donator/",name="Female Donor 4",metaDownloadFalse=true},
+		{id=110004,base_id=55,path="models/peds/female/donator/",name="Female Donor 5",metaDownloadFalse=true},
+		{id=110005,base_id=55,path="models/peds/female/donator/",name="Female Donor 6",metaDownloadFalse=true},
+		{id=110006,base_id=55,path="models/peds/female/donator/",name="Female Donor 7",metaDownloadFalse=true},
+		{id=110007,base_id=55,path="models/peds/female/donator/",name="Female Donor 8",metaDownloadFalse=true},
+		{id=110008,base_id=55,path="models/peds/female/donator/",name="Female Donor 9",metaDownloadFalse=true},
+		{id=110009,base_id=55,path="models/peds/female/donator/",name="Female Donor 10",metaDownloadFalse=true},
+		{id=110010,base_id=55,path="models/peds/female/donator/",name="Female Donor 11",metaDownloadFalse=true},
+		
+		--female job
+		{id=115000,base_id=55,path="models/peds/female/job/",name="Female Job Construction",metaDownloadFalse=true},
+		
+		--female team
+		{id=180000,base_id=246,path="models/peds/female/team/",name="Female SAPD 1",metaDownloadFalse=true},
+		{id=180001,base_id=246,path="models/peds/female/team/",name="Female FIB 1",metaDownloadFalse=true},
+		{id=180002,base_id=275,path="models/peds/female/team/",name="Female SAMD 1",metaDownloadFalse=true},
+		{id=180003,base_id=207,path="models/peds/female/team/",name="Female Grove 1",metaDownloadFalse=true},
+		{id=180004,base_id=207,path="models/peds/female/team/",name="Female Grove 2",metaDownloadFalse=true},
+		
+		
+		
+		
+		
+		
+		--male
+		{id=105000,base_id=19,path="models/peds/male/",name="Male 1",metaDownloadFalse=true},
+		{id=105001,base_id=19,path="models/peds/male/",name="Male 2",metaDownloadFalse=true},
+		{id=105002,base_id=19,path="models/peds/male/",name="Male 3",metaDownloadFalse=true},
+		{id=105003,base_id=25,path="models/peds/male/",name="Male 4",metaDownloadFalse=true},
+		{id=105004,base_id=25,path="models/peds/male/",name="Male 5",metaDownloadFalse=true},
+		{id=105005,base_id=25,path="models/peds/male/",name="Male 6",metaDownloadFalse=true},
+		{id=105006,base_id=25,path="models/peds/male/",name="Male 7",metaDownloadFalse=true},
+		{id=105007,base_id=25,path="models/peds/male/",name="Male 8",metaDownloadFalse=true},
+		{id=105008,base_id=25,path="models/peds/male/",name="Male 9",metaDownloadFalse=true},
+		{id=105009,base_id=25,path="models/peds/male/",name="Male 10",metaDownloadFalse=true},
+		{id=105010,base_id=25,path="models/peds/male/",name="Male 11",metaDownloadFalse=true},
+		{id=105011,base_id=25,path="models/peds/male/",name="Male 12",metaDownloadFalse=true},
+		{id=105012,base_id=25,path="models/peds/male/",name="Male 13",metaDownloadFalse=true},
+		{id=105013,base_id=25,path="models/peds/male/",name="Male 14",metaDownloadFalse=true},
+		{id=105014,base_id=25,path="models/peds/male/",name="Male 15",metaDownloadFalse=true},
+		{id=105015,base_id=25,path="models/peds/male/",name="Male 16",metaDownloadFalse=true},
+		{id=105016,base_id=25,path="models/peds/male/",name="Male 17",metaDownloadFalse=true},
+		{id=105017,base_id=25,path="models/peds/male/",name="Male 18",metaDownloadFalse=true},
+		{id=105018,base_id=25,path="models/peds/male/",name="Male 19",metaDownloadFalse=true},
+		{id=105019,base_id=25,path="models/peds/male/",name="Male 20",metaDownloadFalse=true},
+		{id=105020,base_id=25,path="models/peds/male/",name="Male 21",metaDownloadFalse=true},
+		{id=105021,base_id=25,path="models/peds/male/",name="Male 22",metaDownloadFalse=true},
+		{id=105022,base_id=25,path="models/peds/male/",name="Male 23",metaDownloadFalse=true},
+		{id=105023,base_id=25,path="models/peds/male/",name="Male 24",metaDownloadFalse=true},
+		{id=105024,base_id=25,path="models/peds/male/",name="Male 25",metaDownloadFalse=true},
+		{id=105025,base_id=25,path="models/peds/male/",name="Male 26",metaDownloadFalse=true},
+		
+		--male donor
+		{id=106000,base_id=169,path="models/peds/male/donator/",name="Male Donor 1",metaDownloadFalse=true},
+		{id=106001,base_id=25,path="models/peds/male/donator/",name="Male Donor 2",metaDownloadFalse=true},
+		{id=106002,base_id=25,path="models/peds/male/donator/",name="Male Donor 3",metaDownloadFalse=true},
+		{id=106003,base_id=25,path="models/peds/male/donator/",name="Male Donor 4",metaDownloadFalse=true},
+		{id=106004,base_id=25,path="models/peds/male/donator/",name="Male Donor 5",metaDownloadFalse=true},
+		{id=106005,base_id=25,path="models/peds/male/donator/",name="Male Donor 6",metaDownloadFalse=true},
+		{id=106006,base_id=25,path="models/peds/male/donator/",name="Male Donor 7",metaDownloadFalse=true},
+		{id=106007,base_id=25,path="models/peds/male/donator/",name="Male Donor 8",metaDownloadFalse=true},
+		
+		--male team
+		
+		{id=185000,base_id=286,path="models/peds/male/team/",name="Male FIB 1",metaDownloadFalse=true},
+		{id=185001,base_id=286,path="models/peds/male/team/",name="Male FIB 2",metaDownloadFalse=true},
+		
+		
+		
+		
+		
+		
+		
+		{id=200000,base_id=25,path="models/peds/admin/",name="Lunny",metaDownloadFalse=true},
+	},
+	
+	vehicle={
+		{id=85000,base_id=415,path="models/vehicles/",name="Pfister Comet XLR8",metaDownloadFalse=true},
+		{id=85001,base_id=560,path="models/vehicles/",name="Karin Sultan Hatchback",metaDownloadFalse=true},
+		
+		{id=80000,base_id=531,path="models/vehicles/job/",name="Tactor",metaDownloadFalse=true},
+		{id=80001,base_id=408,path="models/vehicles/job/",name="Trashmaster",metaDownloadFalse=true},
+	},
+	
+	object={
+		{id=8000000,base_id=1025,lodDistance=200,path={txd="models/objects/wheels/Wheel.txd",dff="models/objects/wheels/Wheel_1.dff",col=nil},name="Wheel 1",metaDownloadFalse=true},
+	},
+}
